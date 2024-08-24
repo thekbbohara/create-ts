f9763f99ee399416a2795fccb3778630f8b77a6f
kbzcraft <ytkbzcraft@gmail.com>
2024-08-24 18:14:55 +0545

    add npm support with configFile to save default

--- src/index.ts
+++ src/index.ts
@@ -11,49 +11,66 @@ import { input } from "@inquirer/prompts";
 import { createSpinner } from "nanospinner";
 const sleep = (ms = 200) => new Promise((r) => setTimeout(r, ms));
 const cwd = process.cwd();
-
+const CONFIGPATH = "./createConfig.json";
+const CONFIG = JSON.parse(fs.readFileSync(CONFIGPATH, "utf8"));
+const DPM = CONFIG["default_package_manager"] || "pnpm";
 const execCommand = async (command: string, cwd: string) => {
   const spinner = createSpinner(`${command}`).start();
-  spinner.spin();
   exec(command, { cwd: cwd });
+  await sleep();
   spinner.success();
 };
-const main = async () => {
-  if (process.argv.length === 2) {
-    const devDependencies = [
-      "typescript",
-      "@types/node",
-      "prettier",
-      "eslint",
-      "eslint-config-prettier ",
-      "eslint-plugin-prettier ",
-      "@typescript-eslint/eslint-plugin ",
-      "@typescript-eslint/parser",
-      "nodemon",
-    ];
-    const dirName = await getDirName();
-    const appDir = joinPath(cwd, dirName);
-    console.log(chalk.blue("dir:"), appDir);
-    fs.mkdirSync(appDir);
-    execCommand("pnpm init", appDir);
-    await sleep();
-    execCommand("git init", appDir);
-    installDev(devDependencies, appDir);
-    writePrettierrc(appDir);
-    writeEslint(appDir);
-    writeTsConf(appDir);
-    addGitIgnore(appDir);
-    execCommand('npm pkg set scripts.build="tsc"', appDir);
-    execCommand('npm pkg set scripts.dev="npx nodemon"', appDir);
-    execCommand('npm pkg set scripts.start="ts-node src/index.ts"', appDir);
-    addNodemon(appDir);
-    fs.mkdirSync(joinPath(appDir, "src"));
-    fs.writeFileSync(
-      joinPath(appDir, "src/index.ts"),
-      "console.log('Hello World!')",
-    );
-    setupSuccessLog();
+const main = async (pm: string) => {
+  const devDependencies = [
+    "typescript",
+    "@types/node",
+    "prettier",
+    "eslint",
+    "eslint-config-prettier ",
+    "eslint-plugin-prettier ",
+    "@typescript-eslint/eslint-plugin ",
+    "@typescript-eslint/parser",
+    "nodemon",
+  ];
+  const dirName = await getDirName();
+  const appDir = joinPath(cwd, dirName);
+  console.log(chalk.bgBlue.black("dir:"), chalk.yellow(appDir));
+  fs.mkdirSync(appDir);
+  switch (pm) {
+    case "npm":
+      await execCommand(`${pm} init -y`, appDir);
+      break;
+    case "pnpm":
+      await execCommand(`${pm} init `, appDir);
+      break;
+    default:
+      console.error(
+        "invalid package manager, unable to initialize project:",
+        pm,
+      );
+      process.exit(1);
   }
+
+  await sleep();
+  await execCommand("git init", appDir);
+  installDev(pm, devDependencies, appDir);
+  writePrettierrc(appDir);
+  writeEslint(appDir);
+  writeTsConf(appDir);
+  addGitIgnore(appDir);
+
+  await execCommand(`${pm} pkg set scripts.build="tsc"`, appDir);
+  await execCommand(`${pm} pkg set scripts.dev="npx nodemon"`, appDir);
+  await execCommand(`${pm} pkg set scripts.start="ts-node src/index.ts"`, appDir);
+
+  addNodemon(appDir);
+  fs.mkdirSync(joinPath(appDir, "src"));
+  fs.writeFileSync(
+    joinPath(appDir, "src/index.ts"),
+    "console.log('Hello World!')",
+  );
+  setupSuccessLog();
+  process.exit(0);
 };
 
 const validateDirname = (value: string): true | string => {
@@ -75,11 +92,23 @@ const getDirName = async () => {
   return answer;
 };
 
-const installDev = (depen: string[], cwd: string) => {
+const installDev = (pm: string, depen: string[], cwd: string) => {
   depen.forEach((d) => {
     const spinner = createSpinner(`+ ${chalk.blueBright(d)}`).start();
-    spinner.spin();
-    exec(`pnpm add -D ${d}`, { cwd: cwd });
+    switch (pm) {
+      case "pnpm":
+        exec(`pnpm add -D ${d}`, { cwd: cwd });
+        break;
+      case "npm":
+        exec(`npm i -D ${d}`, { cwd: cwd });
+        break;
+      default:
+        console.error(
+          "invalid package manager, unable to install devDependencies",
+          pm,
+        );
+        process.exit(1);
+    }
     spinner.success();
   });
 };
@@ -87,7 +116,6 @@ const installDev = (depen: string[], cwd: string) => {
 const writeFile = (dirPath: string, fileName: string, data: string) => {
   const spinner = createSpinner(`Writing ${fileName}`).start();
   try {
-    spinner.spin();
     fs.writeFileSync(joinPath(dirPath, fileName), data);
     spinner.success();
   } catch (err) {
@@ -115,22 +143,36 @@ const writeEslint = (dirPath: string) => {
 };
 const writeTsConf = (dirPath: string) => {
   const data = `{
-    "compilerOptions": {
-        "target": "ES2019",
-        "module": "CommonJS",
-        "moduleResolution": "node",
-        "skipLibCheck": true,
-        "resolveJsonModule": true,
-        "esModuleInterop": true,
-        "emitDecoratorMetadata": true,
-        "experimentalDecorators": true,
-        "allowSyntheticDefaultImports": true,
-        "declaration": false,
-        "outDir": "dist"
-    },
-    "include": ["src"],
-    "exclude": ["node_modules", "**/*.spec.ts"]
-}`;
+  "compilerOptions": {
+    "target": "ES2023",
+    "module": "NodeNext",
+    "allowJs": true,
+    "moduleResolution": "NodeNext",
+    "skipLibCheck": true,
+    "resolveJsonModule": true,
+    "esModuleInterop": true,
+    "emitDecoratorMetadata": true,
+    "experimentalDecorators": true,
+    "allowSyntheticDefaultImports": true,
+    "declaration": false,
+    "outDir": "./dist",
+    "rootDir": "./src",
+    "strict": true, // Enable all strict type-checking options
+    "noImplicitAny": true, // Raise error on expressions with an implicit 'any' type
+    "strictNullChecks": true, // Ensure that null and undefined are handled explicitly
+    "strictFunctionTypes": true, // Check function parameter bivariance
+    "strictPropertyInitialization": true, // Ensure class properties are correctly initialized
+    "noImplicitThis": true, // Raise error on 'this' expressions with an implicit 'any' type
+    "alwaysStrict": true, // Enforce strict mode in all files
+    "noUnusedLocals": true, // Report errors on unused local variables
+    "noUnusedParameters": true, // Report errors on unused function parameters
+    "noImplicitReturns": true, // Ensure that all code paths in a function return a value
+    "noFallthroughCasesInSwitch": true // Ensure switch statements have a default case or handle all possible values
+  },
+  "include": ["src/**/*"],
+  "exclude": ["node_modules", "**/*.spec.ts"]
+}
+`;
   writeFile(dirPath, "tsconfig.json", data);
 };
 
@@ -180,4 +222,41 @@ ${chalk.gray("For more information or to contribute to the project, visit the re
 ${chalk.blue("https://github.com/thekbbohara/create-ts")}
 `);
 };
-main();
+
+const validFlag: string[] = ["--npm", "--pnpm", "--default"];
+const validPM: string[] = ["npm", "pnpm"];
+// --- execution --- //
+const args = process.argv;
+if (args.length <= 2) {
+  main(DPM);
+} else {
+  const flag = args[2];
+  if (args.length === 3 && validFlag.includes(flag)) {
+    switch (flag) {
+      case "--npm":
+        await main("npm");
+        break;
+      case "--pnpm":
+        await main("pnpm");
+        break;
+      case "--default":
+        console.log(DPM);
+        process.exit(1);
+    }
+  } else if (
+    args.length === 4 &&
+    validFlag.includes(flag) &&
+    validPM.includes(args[3])
+  ) {
+    CONFIG["default_package_manager"] = args[3];
+    const configStr = JSON.stringify(CONFIG);
+    fs.writeFileSync(CONFIGPATH, configStr);
+    console.info("Changed default package manager to:", args[3]);
+    process.exit(0);
+  } else {
+    console.error(chalk.red("Invalid arguments."));
+    console.log(chalk.magenta("Valid flags"), validFlag);
+    console.log(chalk.magenta("supported package managers"), validPM);
+    process.exit(1);
+  }
+}
