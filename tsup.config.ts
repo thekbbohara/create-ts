6bb3709f7be85fc0100d299f7d1500f85ae64c34
kbzcraft <ytkbzcraft@gmail.com>
2024-08-24 12:59:06 +0545

    removed unnecessary build file

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   outDir: "dist",
-  format: ["cjs", "esm"],
-  sourcemap: true,
+  format: ["esm"],
+  // sourcemap: true,
   clean: true,
-  dts: true, // Generates .d.ts files for TypeScript types
+  dts: false, // Generates .d.ts files for TypeScript types
 });
